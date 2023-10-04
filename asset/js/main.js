42bb3f37c0e74f0fa78ebbe6ffb88444fccda000
Aaliiyuu <aliumukhtar123@gmail.com>
2023-10-04 18:11:43 +0100

    modification of services and nav-bar

--- asset/js/main.js
+++ asset/js/main.js
@@ -34,3 +34,11 @@ $(document).ready(function(){
         }
     });
 });
+function myFunction() {
+    var x = document.getElementById("nav-bar");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
